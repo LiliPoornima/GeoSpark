09ba7fafd2d7d585e57765279fb42d40bf192015
Poornima Liyanage <n.poornimaliyanage@gmail.com>
2025-10-14 15:39:58 +0530

    new

---
+++ frontend/src/test-imports.ts
@@ -0,0 +1,12 @@
+// Test file to verify imports
+import { SiteAnalysis } from './pages/SiteAnalysis'
+import { ResourceEstimation } from './pages/ResourceEstimation'
+import { CostEvaluation } from './pages/CostEvaluation'
+import { Reports } from './pages/Reports'
+
+console.log('All imports successful:', {
+  SiteAnalysis,
+  ResourceEstimation,
+  CostEvaluation,
+  Reports
+})
